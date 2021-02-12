57c331904ddec0501990dca767b329ed1c42f360
jacklmbrt07 <jacklmbrt07@gmail.com>
2021-02-12 11:09:43 -0800

    gettin somewhere

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -1,3 +1,4 @@
+from matplotlib import lines
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
@@ -24,11 +25,23 @@ def draw_plot():
     x_future = df["Year"]
     y_res = res.intercept + res.slope * x_future
     plt.plot(x_future, y_res, 'r', label='fitted line')
-    plt.plot(x, y, 'r', label='fitted line')
-    
-
 
     # Create second line of best fit
+    x2 = df[(df["Year"] >= 2000) & (df["Year"] <= 2013)]["Year"]
+    y2 = df[(df["Year"] >= 2000) & (df["Year"] <= 2013)]["CSIRO Adjusted Sea Level"]
+    res2 = linregress(x2, y2)
+    
+    last_year = 2013
+    future = range(2050-last_year)
+    for i in future:
+        i += 1
+        new_year = (last_year+i)
+        new_y = res2.slope*new_year + res2.intercept
+        df = df.append({"Year": int(new_year), "CSIRO Adjusted Sea Level": new_y}, ignore_index=True)
+        
+    x_future2 = df[(df["Year"] >= 2000) & (df["Year"] <= 2050)]["Year"]
+    y_res2 = res2.intercept + res2.slope * x_future2
+    plt.plot(x_future2, y_res2, 'r', label='fitted line')
 
     # Add labels and title
 
